625331d60bc68c9e885cb153b4cb2d6c009bfb24
Anirudh Vadlamani <anirudhvadlamani2001@gmail.com>
2023-02-07 10:02:24 +0400

    initial commit

---
+++ service-worker.js
@@ -0,0 +1,34 @@
+const cacheName = 'petstore-v1';
+const cacheFiles = [
+  'index.html',
+  'products.js',
+  'petstore.webmanifest',
+  // 'images/icon-512.png',
+  // 'images/product.png',
+  // 'images/product2.png',
+  // 'images/product3.png',
+  // 'images/product4.jpg',
+  // 'images/product5.jpg',
+  'images/',
+];
+
+self.addEventListener('install', (e) => {
+  console.log('[Service Worker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      console.log('[Service Worker] Caching all the files');
+      return cache.addAll(cacheFiles);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((r) => {
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+
+      // 'r' is the matching file if it exists in the cache
+      return r;
+    })
+  );
+});
